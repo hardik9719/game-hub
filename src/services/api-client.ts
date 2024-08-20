a259b623f56fb6dd064ed17e79273ba2d23a2d83
Hardik Tanna <hardik.tanna@somaiya.edu>
2024-08-20 17:48:56 -0400

    Connect to Express Proxy Vercel

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,6 +1,6 @@
 import axios, {CanceledError} from "axios";
 export default axios.create({
-    baseURL:"/api",
+    baseURL:"https://express-proxy-3bksjzsth-hardik-tannas-projects.vercel.app",
     headers: {
         "Content-Type": "text/plain", // Set content type to text/plain
       },

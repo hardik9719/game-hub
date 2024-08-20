3403abd8cb18c7c1d697f1943da80036e41229c6
Hardik Tanna <hardik.tanna@somaiya.edu>
2024-08-20 05:03:41 -0400

    Ship genres with app

---
+++ src/data/genre.ts
@@ -0,0 +1,52 @@
+export default [
+    {
+      "id": 36,
+      "name": "MOBA",
+      "slug": "moba"
+    },
+    {
+      "id": 2,
+      "name": "Point-and-click",
+      "slug": "point-and-click"
+    },
+    {
+      "id": 4,
+      "name": "Fighting",
+      "slug": "fighting"
+    },
+    {
+      "id": 5,
+      "name": "Shooter",
+      "slug": "shooter"
+    },
+    {
+      "id": 7,
+      "name": "Music",
+      "slug": "music"
+    },
+    {
+      "id": 8,
+      "name": "Platform",
+      "slug": "platform"
+    },
+    {
+      "id": 9,
+      "name": "Puzzle",
+      "slug": "puzzle"
+    },
+    {
+      "id": 10,
+      "name": "Racing",
+      "slug": "racing"
+    },
+    {
+      "id": 11,
+      "name": "Real Time Strategy (RTS)",
+      "slug": "real-time-strategy-rts"
+    },
+    {
+      "id": 12,
+      "name": "Role-playing (RPG)",
+      "slug": "role-playing-rpg"
+    }
+  ]
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,9 +1,9 @@
-import useData from "./useData";
 import { Entity } from "./useIGDBGame";
+import genres from '../data/genre'
 export interface Genre extends Entity{
   name: string;
   slug:string;
 }
 
-export const useGenres = (body:string | null) => useData<Genre>('/genres',{data:body},[])
+export const useGenres = (body:string | null) => ({data:genres,isLoading:false,error:null})
 export default useGenres;

95c77aa1331fc50491f12db284a3e01012c60cd8
Hardik Tanna <hardik.tanna@somaiya.edu>
2024-08-24 02:59:08 -0400

    Only to test vercel with cors check

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,31 @@
 import axios, {CanceledError} from "axios";
-export default axios.create({
-    baseURL:"https://express-proxy-3bksjzsth-hardik-tannas-projects.vercel.app",
-    headers: {
-        "Content-Type": "text/plain", // Set content type to text/plain
-      },
-})
+
+const tokenData= {
+  baseURL:"https://api.igdb.com/v4",
+  params:{
+      clientId: "nz6zfkre0wra9k6hgecit3sjo77hkv",
+      clientSecret: "r5192bua37c9bzjfnjiij4gha48qmq",
+      token:{
+          "access_token": "c2ajx9tfczz9uvnokp1fyv1w81zsu6",
+          "expires_in": 5217430,
+          "token_type": "bearer"
+      }
+  }}
+export default axios.create(
+  {
+      baseURL:"https://api.igdb.com/v4",
+      headers:{
+          "Client-ID":tokenData.params.clientId,
+          "Authorization": "Bearer "+tokenData.params.token.access_token,
+          "Content-Type": "application/x-www-form-urlencoded"
+      }
+  }
+)
+
+// export default axios.create({
+//     baseURL:"https://express-proxy-3bksjzsth-hardik-tannas-projects.vercel.app",
+//     headers: {
+//         "Content-Type": "text/plain", // Set content type to text/plain
+//       },
+// })
 export {CanceledError}
